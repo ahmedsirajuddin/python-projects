2062e08c44bf8d4621b538d4b752cc526ea5f1da
Ayman <ahmed.a.sirajuddin@gmail.com>
2015-06-15 00:59:48 -0400

    updated LinkedList and unit_test.py. Added comments to LinkedList.py as well.

--- Linked-List/LinkedList.py
+++ Linked-List/LinkedList.py
@@ -1,22 +1,39 @@
 class LinkedList:
+    '''
+    A singly linked list.
+    '''
     def __init__(self, head=None):
-        self.head = head
+        if head:
+            self.head = Node(head)
+        else:
+            self.head = head
 
     def __str__(self):
         current = self.head
+        result = "["
         if current:
-            return current.__str__()
-        return "[]"
+            result += current.__str__()
+        return result + "]"
 
 
     def is_empty(self):
+        '''
+        Check if the linked list is empty.
+        '''
         return self.head == None
 
-    def insert_beginning(self, node):
+    def insert_beginning(self, item):
+        '''
+        Inserts a Node to the beginning of the list.
+        '''
+        node = Node(item)
         node.set_next(self.head)
         self.head = node
 
     def size(self):
+        '''
+        Get the size of the linked list.
+        '''
         current = self.head
         size = 0
         if current:
@@ -27,37 +44,108 @@ class LinkedList:
         return size
 
     def search(self, item):
+        '''
+        Searches the list for a Node which has data matching 'item'.
+        '''
         current = self.head
-        if current:
-            while current.get_data() != item:
-                if current.get_next():
-                    current = current.get_next()
-                else:
-                    return False
-        else:
-            return False
-        return True
+        found = False
+        while current and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
 
+        return found
+
+    def remove(self, item):
+        '''
+        Removes the node in item whose data is equal to 'item'. Return nothing if node does not
+        exist.
+        '''
+        current = self.head
+        prev = None
+        found = False
+
+        while current and not found:
+            if current.get_data() == item:
+                found = True
+                if prev:
+                    prev.set_next(current.get_next())
+                else:
+                    self.head = current.get_next()
+            else:
+                prev = current
+                current = current.get_next()
+        
+        return found
+        
+        if prev:
+            prev.set_next(current.get_next())
+
+        else: # self.head is the node we want to remove
+            self.head = current.get_next()
+
+    def index_of(self, item):
+        '''
+        Checks if a node exists whose data is equal to item. If it does exist, return the
+        index of that node (index starts at 0, meaning that the first item on the list has
+        an index of 0). If it does not exist, return false.
+        '''
+        current = self.head
+        found = False
+        index = 0
+
+        while current and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                index += 1
+                current = current.get_next()
+
+        if found:
+            return index
+
+        return False
+                
+
+        
 class Node:
+    '''
+    Node for a singly linked list.
+    '''
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
     def __str__(self):
         node = self
+        result = ""
         if node.next:
             node = node.next
-            return str(self.data) + node.__str__() 
+            return str(self.data) + ", " + node.__str__()
         return str(self.data)
 
     def get_data(self):
+        '''
+        Get the data of a node.
+        '''
         return self.data
 
     def get_next(self):
+        '''
+        Get the next item which the given node links to (could be another node or could
+        be None - None if the given node is the tail of the linked list).
+        '''
         return self.next
 
     def set_data(self, new_data):
+        '''
+        Set the data of the given node.
+        '''
         self.data = new_data
 
     def set_next(self, next_node):
+        '''
+        Set the next item of a given node.
+        '''
         self.next = next_node
--- Linked-List/unit_test.py
+++ Linked-List/unit_test.py
@@ -6,11 +6,12 @@ class NodeFunctionsTest(unittest.TestCase):
     def setUp(self):
         self.n1 = Node(1)
         self.n2 = Node(2)
-        self.n3 = Node(3, self.n2)
+        self.n3 = Node(3)
+        self.n3.next = self.n2
 
     def test_get_data(self):
-        self.assertEqual(self.n1.__str__(), "1")
-        self.assertEqual(self.n3.__str__(), "32")
+        self.assertEqual(self.n1.get_data(), 1)
+        self.assertEqual(self.n3.get_data(), 3)
 
     def test_get_next(self):
         self.assertEqual(self.n1.get_next(), None)
@@ -18,44 +19,68 @@ class NodeFunctionsTest(unittest.TestCase):
 
     def test_set_data(self):
         self.n1.set_data(11)
-        self.assertEqual(self.n1.get_data(), 11)
+        self.assertEqual(self.n1.data, 11)
     
     def test_set_next(self):
         self.n2.set_next(self.n1)
-        self.assertEqual(self.n2.get_next(), self.n1)
+        self.assertEqual(self.n2.next, self.n1)
 
 class ListFunctionsTest(unittest.TestCase):
 
     def setUp(self):
-        self.l0 = LinkedList()
-        
         self.l1 = LinkedList()
-        
-        self.n1 = Node(1)
-        self.n2 = Node(2)
-        self.n3 = Node(3)
-        self.l2 = LinkedList(self.n1)
+        self.l2 = LinkedList(2)
+        self.l3 = LinkedList(3)
 
     def test_is_empty(self):
-        self.assertTrue(self.l1.is_empty())
-        self.assertFalse(self.l2.is_empty())
+        self.assertEqual(self.l1.head, None)
+        self.assertFalse(self.l2.head == None)
 
     def test_insert_beginning(self):
-        self.l1.insert_beginning(self.n1)
-        self.assertEqual(self.l1.__str__(), "1")
+        self.l1.insert_beginning(1)
+        self.assertEqual(self.l1.head.data, 1)
+        self.assertEqual(self.l1.head.next, None)
+
+        self.l2.insert_beginning(1)
+        self.assertEqual(self.l2.head.data, 1)
+        self.assertEqual(self.l2.head.next.data, 2)
 
     def test_size(self):
-        self.assertEqual(self.l0.size(), 0)
-
-        self.l1.insert_beginning(self.n1)
-        self.assertEqual(self.l1.size(), 1)
-        
+        self.assertEqual(self.l1.size(), 0)
         self.assertEqual(self.l2.size(), 1)
 
     def test_search(self):
-        self.assertFalse(self.l0.search(1))
-        self.assertFalse(self.l1.search(2))
-        self.assertTrue(self.l2.search(1))
+        self.assertFalse(self.l1.search(1))
+        self.assertTrue(self.l2.search(2))
+
+    def test_remove(self):
+        self.l2.remove(2)
+        self.assertEqual(self.l2.head, None)
+
+        self.assertFalse(self.l1.remove(1))
+
+        self.l3.insert_beginning(2)
+        self.l3.insert_beginning(1)
+        self.l3.remove(2)
+
+    def test_index_of(self):
+        self.myList = LinkedList()
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        self.myList.head = node1
+        node1.next = node2
+        node2.next = node3
+        
+        self.assertEqual(self.myList.index_of(1), 0)
+        self.assertEqual(self.myList.index_of(2), 1)
+        self.assertEqual(self.myList.index_of(3), 2)
+
+        self.assertFalse(self.l1.index_of(1))
+        self.assertFalse(self.l2.index_of(1))
+        self.assertEqual(self.l3.index_of(1), 0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
